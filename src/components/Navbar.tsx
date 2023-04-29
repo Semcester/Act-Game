d8f0c0668fbb7114674c91e0b3c3923f0c8c3c25
Semih Senan <senansemih@gmail.com>
2023-04-29 18:07:38 +0300

    Color switch added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../public/assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image boxSize="60px" src={logo} />
-      <Text>Selam</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
